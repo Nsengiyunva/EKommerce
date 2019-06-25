1a9f3f2c1c00cab0a1537ecb977b9713af3bfa5b
Nsengiyunva King Isaac <kingbecks07@gmail.com>
2019-06-25 20:42:37 +0300

    setup for the login

---
+++ app/screens/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './LoginStyles.js'
+
+class extends React.Component {
+  render(){
+    <View style={styles.container}>
+      <Text>Login</Text>
+    </View>
+  }
+}
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import Login from './app/screens/Login'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
